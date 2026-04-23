56d44ad8e4fd246fc11b525f98d8db2a771f272f
morheat <mpicoopstudent@gmail.com>
2026-04-23 14:05:18 -0400

    deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import svgr from "vite-plugin-svgr";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-export default defineConfig(({ mode }) => ({
-  plugins: [react(), svgr()],
-  base: mode === "production" ? "/ScrewplugHeater/" : "/",
-}));
+export default defineConfig({
+  plugins: [react()],
+  base: '/ScrewplugHeater/', // 👈 THIS IS CRITICAL
+})
